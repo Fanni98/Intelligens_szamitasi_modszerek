1d7e73eed471e65a02f90ddcc84e54df601325cb
Fanni98 <gozfanni@gmail.com>
2022-05-08 16:34:06 +0200

    intelligens szamitasi modszerek feladat

---
+++ install/learning_tf2_py/lib/python3.8/site-packages/learning_tf2_py/turtle_tf2_listener.py
@@ -0,0 +1,112 @@
+import math
+
+from geometry_msgs.msg import Twist
+
+import rclpy
+from rclpy.node import Node
+
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+from math import atan2
+from turtlesim.srv import Spawn
+
+
+class FrameListener(Node):
+
+    def __init__(self):
+        super().__init__('turtle_tf2_frame_listener')
+
+        self.declare_parameter('target_frame', 'turtle1')
+        self.target_frame = self.get_parameter(
+            'target_frame').get_parameter_value().string_value
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+        
+        self.spawner = self.create_client(Spawn, 'spawn')
+        self.turtle_spawning_service_ready = False
+        self.turtle_spawned = False
+
+        self.publisher = self.create_publisher(Twist, 'turtle2/cmd_vel', 1)
+
+        self.timer = self.create_timer(1.0, self.on_timer)
+
+    def on_timer(self):
+        from_frame_rel = self.target_frame
+        to_frame_rel = 'turtle2'
+
+        if self.turtle_spawning_service_ready:
+            if self.turtle_spawned:
+                try:
+                    now = rclpy.time.Time()
+                    trans = self.tf_buffer.lookup_transform(
+                        to_frame_rel,
+                        from_frame_rel,
+                        now)
+                except TransformException as ex:
+                    self.get_logger().info(
+                        f'Could not transform {to_frame_rel} to {from_frame_rel}: {ex}')
+                    return
+
+                msg = Twist()
+
+                turn =   math.atan2(trans.transform.translation.y,trans.transform.translation.x)
+                distance = math.sqrt(trans.transform.translation.x ** 2 +trans.transform.translation.y ** 2)
+
+                #tavolsag alapjan valtozo sebesseg
+                if distance > 0.1:
+                    scale_forward_speed = 0.1
+                    scale_turn_speed = 0.1
+                    msg.linear.x = scale_forward_speed*distance
+                    msg.angular.z = scale_turn_speed*turn
+                if distance > 0.2:
+                    scale_forward_speed = 0.3
+                    scale_turn_speed = 0.3
+                    msg.linear.x = scale_forward_speed*distance
+                    msg.angular.z = scale_turn_speed*turn
+                if distance > 0.3:
+                    scale_forward_speed = 0.7
+                    scale_turn_speed = 0.7
+                    msg.linear.x = scale_forward_speed*distance
+                if distance > 0.4:
+                    scale_forward_speed = 0.9
+                    scale_turn_speed = 0.7
+                    msg.linear.x = scale_forward_speed*distance
+                    msg.angular.z = scale_turn_speed*turn
+                else:
+                    scale_forward_speed = 1.0
+                    scale_turn_speed = 1.0
+                    msg.linear.x = scale_forward_speed*distance
+                    msg.angular.z = scale_turn_speed*turn
+
+                self.publisher.publish(msg)
+            else:
+                if self.result.done():
+                    self.get_logger().info(
+                        f'Successfully spawned {self.result.result().name}')
+                    self.turtle_spawned = True
+                else:
+                    self.get_logger().info('Spawn is not finished')
+        else:
+            if self.spawner.service_is_ready():
+                request = Spawn.Request()
+                request.name = 'turtle2'
+                request.x = float(4)
+                request.y = float(2)
+                request.theta = float(0)
+                self.result = self.spawner.call_async(request)
+                self.turtle_spawning_service_ready = True
+            else:
+                self.get_logger().info('Service is not ready')
+
+
+def main():
+    rclpy.init()
+    node = FrameListener()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    rclpy.shutdown()
